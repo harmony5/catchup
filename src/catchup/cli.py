453bbbeebd80a6cfbc6192e6001457085d21aaf0
jcgo <jeancgo@pm.me>
2021-06-13 03:32:59 -0400

    Updated CLI commands to work with new embedded DB - Added `show` functionality to list all current and completed to-do's. - `add` and `show` now show all current to-do's in new tabulated format.

--- src/catchup/cli.py
+++ src/catchup/cli.py
@@ -1,42 +1,57 @@
 """CLI tool to manage your to-do's.
 """
 
-from os import write
 import typer
-import csv
+from tabulate import tabulate as tab
+import catchup.db as db
 
 
 cli = typer.Typer()
 
 
 @cli.command()
+def show():
+    """Show all to-do's.
+    """
+    todos = list(db.get_todos())
+    typer.echo(tab(todos, headers="keys"))
+
+
+@cli.command()
 def add(todo: str):
     """Add new to-do.
     """
-    with open("todos.txt", "a") as todo_file:
-        id = str(abs(hash(todo)))[:5]
-        writer = csv.writer(todo_file, "unix")
-        writer.writerow([id, todo, "In Progress"])
+    todo_id = str(hash(todo))[:6]
+    db.add_todo(todo_id, todo)
 
     typer.echo(f"Added {todo} to todos.txt:")
-    with open("todos.txt") as todo_file:
-        todos = csv.reader(todo_file, "unix")
-        for id, desc, status in todos:
-            typer.echo(f"{id:6}\t{desc}\t{status}")
+
+    typer.echo("Id\tDescription\tStatus\tCreated At")
+    for todo in db.get_todos_by_status("In Progress"):
+        typer.echo(
+            f"{todo['id']}\t"
+            f"{todo['description']}\t"
+            f"{todo['status']}\t"
+            f"{todo['creation_date']}"
+        )
 
 
 @cli.command()
-def complete(todo: str):
-    """Mark a specific to-do as completed.
+def complete(todo_id: int):
+    """Mark the to-do specified by <todo_id> as completed.
     """
-    typer.echo(f"Todo [{todo}] was marked as completed.")
+    todo = db.get_todo(todo_id)
+    typer.echo(
+        f"Todo with id #{todo_id} {todo['description']} was marked as completed."
+    )
 
 
 @cli.command()
-def remove(todo: str):
-    """Remove a specific to-do from the list.
+def remove(todo_id: int):
+    """Remove the to-do specified by <todo_id> from the list.
     """
-    typer.echo(f"Removed [{todo}] from todos.txt")
+    todo = db.get_todo(todo_id)
+    typer.echo(f"Removed to-do with id #{todo_id}: {todo['description']}.")
 
 
 if __name__ == "__main__":
