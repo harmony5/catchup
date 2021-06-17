fea868a4ec901d2f317d97fbaf20e7979551b216
jcgo <jeancgo@pm.me>
2021-06-17 19:27:09 -0400

    Added `--version`, `--verbose` and `show --status` options
    * `--verbose` option now activates verbose mode (more detailed output).
    * `--version` (or `-v`) option shows the current version of the app.
    * `catchup add` now only lists to-do's when in **verbose mode**.
    * `--status STATUS` option of `show` subcommand specifies which to-do's to show, according to their corresponding STATUS.
    * Added shell completion (double tab) functionality.

--- src/catchup/cli.py
+++ src/catchup/cli.py
@@ -1,49 +1,99 @@
 """CLI tool to manage your to-do's.
 """
 
+from typing import Optional
 import typer
 from tabulate import tabulate as tab
-import catchup.db as db
+import src.catchup.db as db
+from src.catchup import __version__
 
 
-cli = typer.Typer()
+cli = typer.Typer(help=__doc__)
+
+
+STATE = {"verbose": False}
+VALID_STATUSES = [
+    ('"in progress"', "shows only to-do's in progress."),
+    ("completed", "shows only completed to-do's."),
+]
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"catchup v{__version__}")
+        raise typer.Exit()
+
+
+def status_callback(status: Optional[str]):
+    if status is not None and status.lower() not in ("in progress", "completed"):
+        raise typer.BadParameter(
+            "Status '{status}' not recognized. Must be one of 'in progress' or 'completed'."
+        )
+    return status
+
+
+def status_completion(incomplete: str):
+    for status, help_msg in VALID_STATUSES:
+        if status.startswith(incomplete):
+            yield (status, help_msg)
+
+
+@cli.callback()
+def main(
+    verbose: bool = False,
+    version: bool = typer.Option(None, "--version", "-v", callback=version_callback),
+):
+    if verbose:
+        STATE["verbose"] = True
+        typer.echo("Verbose mode on.")
 
 
 @cli.command()
-def show():
-    """Show all to-do's.
+def show(
+    status: Optional[str] = typer.Option(
+        None,
+        "--status",
+        "-s",
+        help="List only the to-do's with specified status.",
+        callback=status_callback,
+        autocompletion=status_completion,
+    )
+):
+    """Show saved to-do's.
     """
-    todos = list(db.get_todos())
+    if status:
+        todos = db.get_todos_by_status(status.title())
+    else:
+        todos = db.get_todos()
     typer.echo(tab(todos, headers="keys"))
 
 
 @cli.command()
 def add(todo: str):
-    """Add new to-do.
+    """Add new to-do with TODO as the description.
     """
     db.add_todo(todo)
 
     typer.echo(f"Saved new to-do to the Database: {todo}")
-    typer.echo(tab(db.get_todos_by_status("In Progress"), headers="keys"))
+
+    if STATE["verbose"]:
+        typer.echo(tab(db.get_todos_by_status("In Progress"), headers="keys"))
 
 
 @cli.command()
 def complete(todo_id: int):
-    """Mark the to-do specified by <todo_id> as completed.
+    """Mark the to-do specified by TODO_ID as completed.
     """
-    db.update_todo(todo_id, status='Completed')
-    typer.echo(
-        f"Todo with id #{todo_id} was marked as completed."
-    )
+    db.update_todo(todo_id, status="Completed")
+    typer.echo(f"Todo with id #{todo_id} was marked as completed.")
 
 
 @cli.command()
 def remove(todo_id: int):
-    """Remove the to-do specified by <todo_id> from the list.
+    """Remove the to-do specified by TODO_ID from the list.
     """
-    todo = db.get_todo(todo_id)
     db.delete_todo(todo_id)
-    typer.echo(f"Removed to-do with id #{todo_id}: {todo['description']}")
+    typer.echo(f"Removed to-do with id #{todo_id}.")
 
 
 if __name__ == "__main__":
